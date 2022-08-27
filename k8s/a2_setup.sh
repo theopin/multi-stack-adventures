7b4d8a40cf4136342e8fbd76ed1544cf8899c3a7
Theodore Pinto <theopinto98@gmail.com>
2022-08-27 12:48:21 +0800

    Add impln to deploy nginx as development and deploy ingress to expose it to localhost

--- k8s/a2_setup.sh
+++ k8s/a2_setup.sh
@@ -1,2 +1,5 @@
 kind create cluster --name kind-1 --config kind/cluster-config.yaml --image kindest/node:v1.23.0
-kubectl apply -f manifests/backend-deployment.yaml
+kubectl create -f manifests/backend-deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f manifests/backend-service.yaml
+kubectl apply -f manifests/backend-ingress.yaml
