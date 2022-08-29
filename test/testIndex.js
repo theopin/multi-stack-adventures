014b6ded3a6bc3fe15981bca6e5b8060e83f1f00
Theodore Pinto <theopinto98@gmail.com>
2022-08-29 13:55:46 +0800

    refactor get test

--- test/testIndex.js
+++ test/testIndex.js
@@ -8,38 +8,34 @@ chai.use(chaiHttp);
 chai.should();
 
 describe("Accounts REST API", () => {
+    runGetTests();
+
+
+});
+
+function runGetTests() {
     describe("GET /", () => {
         // Test to get all bank user accounts record
         it("should get all bank account users record", (done) => {
-             chai.request(app)
-                 .get('/accounts/')
-                 .end((err, res) => {
-                     res.should.have.status(200);
-                     res.body.should.be.a('object');
-                     done();
-                  })
-         });
+            chai.request(app)
+                .get('/accounts/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
         // Test to get single bank user account record
         it("should get a single bank account user record", (done) => {
-             const id = 1;
-             chai.request(app)
-                 .get(`/accounts/${id}`)
-                 .end((err, res) => {
-                     res.should.have.status(200);
-                     res.body.should.be.a('object');
-                     done();
-                  });
-         });
-         
-        // // Test to get single bank user account record
-        // it("should not get a single bank user account record", (done) => {
-        //      const id = 5;
-        //      chai.request(app)
-        //          .get(`/accounts/${id}`)
-        //          .end((err, res) => {
-        //              res.should.have.status(404);
-        //              done();
-        //           });
-        //  });
+            const id = 1;
+            chai.request(app)
+                .get(`/accounts/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
     });
-});
+}
