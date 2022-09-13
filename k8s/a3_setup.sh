903cc1681c480466fcd71d842b29442b7bd6af35
theopin <theopinto98@gmail.com>
2022-09-13 10:58:06 +0800

    Add modifications to A2.3

--- k8s/a3_setup.sh
+++ k8s/a3_setup.sh
@@ -1,5 +1,24 @@
+printf "\n1a - Applying metric resources...\n"
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+printf "\n2 - Adding flag to enable TLS in metric server...\n"
+# --kubelet-insecure-tls
 kubectl -nkube-system edit deploy/metrics-server
+
+printf "\n3 - Restarting metric server deployment...\n"
 kubectl -nkube-system rollout restart deploy/metrics-server
+
+printf "\n4a - Applying autoscaling...\n"
 kubectl apply -f manifests/backend-hpa.yaml
+
+printf "\n4b - Verify autoscaling...\n"
+kubectl describe hpa
+
+printf "\n5 - Verify zone allocation...\n"
+kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
+
+printf "\n6a - Apply zone-aware scheduling...\n"
 kubectl apply -f manifests/backend-zone-aware.yaml
+
+printf "\n6b - Verify zone-aware scheduling...\n"
+kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
