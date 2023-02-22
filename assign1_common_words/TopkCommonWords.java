38060870829fa353b8893631ddb5fb40e2bb2c90
theopin <theopinto98@gmail.com>
2023-02-22 12:22:26 +0800

    Add updates

--- assign1_common_words/TopkCommonWords.java
+++ assign1_common_words/TopkCommonWords.java
@@ -5,13 +5,10 @@ MATRICULATION NUMBER: A0199277H
 */
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
-import java.util.HashSet;  
 
 
 import org.apache.hadoop.conf.Configuration;
@@ -24,12 +21,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import static java.lang.Integer.parseInt;
+
 public class TopkCommonWords {
 
-    public static HashSet <String> stopwords = new HashSet<String>();  
-    public static int kItrerations;
+    public static HashSet <String> stopwords = new HashSet<String>();
+    public static int kIterations;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "TopkCommonWords");
         job.setJarByClass(TopkCommonWords.class);
@@ -43,45 +42,45 @@ public class TopkCommonWords {
 
         // Read in all stopwords from stopword file (args[2]) and k times to run (args[4])
         scanStopwords(args[2]);
-        kItrerations = parseInt(args[4]);
+        kIterations = parseInt(args[4]);
 
         // Inputs from args[0] and args[1] (file 1 & 2), Output from arg[3] (output file)
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileInputFormat.addInputPath(job, new Path(args[1]));
         FileOutputFormat.setOutputPath(job, new Path(args[3]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-    } 
+    }
 
     public static void scanStopwords(String stopwordFileName) throws FileNotFoundException {
 
-        File stopwordFile = new File(stopwordFile);
-        Scanner fileScanner = new Scanner(text);
-    
+        File stopwordFile = new File(stopwordFileName);
+        Scanner fileScanner = new Scanner(stopwordFile);
+
         while(fileScanner.hasNextLine()){
             String stopword = fileScanner.nextLine();
             if(stopwords.contains(stopword)) {
                 stopwords.add(stopword);
             }
-        }     
+        }
     }
 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
-        private HashMap<String, int> wordCountTracker = new HashMap<String, int>(); 
+        private HashMap<String, Integer> wordCountTracker = new HashMap<String, Integer>();
 
-        public void map(Object key, Text value, Context context) 
+        public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
 
             StringTokenizer itr = new StringTokenizer(value.toString());
 
             while (itr.hasMoreTokens()) {
-            
+
                 String currToken = itr.nextToken();
 
-                // Do not consider words with length less than/equal to 4 or in stopwords list 
+                // Do not consider words with length less than/equal to 4 or in stopwords list
                 if (currToken.length <= 4 || stopwords.contains(currToken)) {
                     continue;
-                }   
+                }
 
                 // Increment present count if token exists, else add to hashmap with init value of 1
                 if (wordCountTracker.containsKey(currToken)) {
@@ -91,29 +90,27 @@ public class TopkCommonWords {
                 }
 
                 // Iterate through all entries in hashmap, emitting <word, count> per pair
-                for (Map.Entry<String,int> wordCountPair : wordCountTracker.entrySet()) {
+                for (Map.Entry<String, Integer> wordCountPair : wordCountTracker.entrySet()) {
                     String key = wordCountPair.getKey();
                     int value = wordCountPair.getValue();
 
                     Text word = new Text(key);
                     IntWritable count = new IntWritable(value);
 
-                    context.write(word, one);
+                    context.write(word, count);
                 }
-                
+
             }
         }
     }
 
     public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-        
-        private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
-            
+
             int minCount = Integer.MAX_VALUE;
-            
+
             // Word does not appear in both files - reject
             if (values.length < 2) {
                 return;
@@ -124,12 +121,16 @@ public class TopkCommonWords {
                     minCount = val.get();
                 }
             }
-            context.write(key, minCount);
+
+            Text word = new Text(key);
+            IntWritable count = new IntWritable(minCount);
+
+            context.write(word, count);
         }
 
-        public void cleanup(Context context) 
+        public void cleanup(Context context)
                 throws IOException, InterruptedException {
-            
+
         }
     }
 }
